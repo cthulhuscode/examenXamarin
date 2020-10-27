b6806f707b8b6ec105fa96e3a6930bf3fd6f25d1
Enrique <jruvalcaba1@ucol.mx>
2020-10-27 15:05:44 -0600

    Arreglé los errores de NavigationPage

---
+++ examen/BaseDeDatos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace examen
+{
+    class BaseDeDatos
+    {        
+        public static string bdName = "tienda1.db";        
+    }
+}
